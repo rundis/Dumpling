c316b30fcd930ac2992cf8ef4d5a240ce0ebeae8
Magnus Rundberget <mrundberget@hotmail.com>
2011-03-04 10:45:21 +0100

    Now it should compile..

--- dumpling-core/src/test/java/com/dumpling/core/DumplingServiceTest.java
+++ dumpling-core/src/test/java/com/dumpling/core/DumplingServiceTest.java
@@ -1,5 +1,6 @@
 package com.dumpling.core;
 
+
 import org.junit.Before;
 import org.junit.Test;
 
--- dumpling-util/src/test/java/com/dumpling/util/StringUtilTest.java
+++ dumpling-util/src/test/java/com/dumpling/util/StringUtilTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
+
 /**
  * Created by: magnus
  */
